66f2b14d514378755521880c2065d5d96ebad564
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-07-17 13:28:13 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/app/components/ui/button"
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-7xl text-green-500">Hello world</h1>
+      <Button>Click here</Button>
     </div>
   )
 }
