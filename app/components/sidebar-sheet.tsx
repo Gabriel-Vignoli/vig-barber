3a5aab4b5e04f14569e72a2fa3695ea4db8f7afc
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-07-23 09:12:53 -0300

    chore: add next auth setup

--- app/components/sidebar-sheet.tsx
+++ app/components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
 import { quickSearchOptions } from "../_constants/search"
 import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SidebarSheet = () => {
   return (
@@ -14,14 +22,42 @@ const SidebarSheet = () => {
       </SheetHeader>
 
       <div className="flex items-center gap-3 border-b px-4 pt-0 pb-5">
-        <Avatar>
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger
+            render={
+              <Button size="icon">
+                <LogInIcon></LogInIcon>
+              </Button>
+            }
+          ></DialogTrigger>
+          <DialogContent className="w-[90%] text-center">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-2 p-4 font-bold">
+              <Image
+                src="/google.svg"
+                alt="Google Icon"
+                width={18}
+                height={18}
+              ></Image>
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="/banner.jpg" alt="User" />
         </Avatar>
 
         <div>
           <p className="font-bold">Gabriel Vignoli</p>
           <div className="text-xs">gabriel.vignoli@example.com</div>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-3 border-b p-4 pt-2">
