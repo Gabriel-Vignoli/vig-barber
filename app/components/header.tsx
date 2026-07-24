73f430221dd0ad05478fa47e384e49ee0436a3ee
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-07-24 12:54:31 -0300

    feat: make logo navigate to home page

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,12 +4,15 @@ import { Button } from "./ui/button"
 import { MenuIcon } from "lucide-react"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card className="bg-background rounded-none">
       <CardContent className="flex items-center justify-between">
-        <Image src="/logo.png" alt="Vig Barber" width={120} height={120} />
+        <Link href="/">
+          <Image src="/logo.png" alt="Vig Barber" width={120} height={120} />
+        </Link>
 
         <Sheet>
           <SheetTrigger
