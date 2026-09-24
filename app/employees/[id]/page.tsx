841a1405de1a78ec415bef8287b734e578b3c995
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-09-24 20:01:09 -0300

    style(employees): add rounded bottom corners to hero image on mobile

--- app/employees/[id]/page.tsx
+++ app/employees/[id]/page.tsx
@@ -121,7 +121,7 @@ const EmployeePage = async ({ params }: EmployeePageProps) => {
           {/* Main column */}
           <div className="lg:col-start-1">
             {/* Image */}
-            <div className="bg-muted relative flex h-62.5 w-full items-center justify-center lg:mx-auto lg:h-96 lg:max-w-md lg:overflow-hidden lg:rounded-xl">
+            <div className="bg-muted relative flex h-62.5 w-full items-center justify-center overflow-hidden rounded-b-3xl lg:mx-auto lg:h-96 lg:max-w-md lg:overflow-hidden lg:rounded-xl">
               {employeeImage ? (
                 <Image
                   src={employeeImage}
